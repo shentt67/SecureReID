31e543551d357e1f9b0e7ea9397f7045a5ecaf3a
shentt67 <1048737191@qq.com>
2023-06-30 11:14:10 +0800

    Update dataset_loader.py

--- data/datasets/dataset_loader.py
+++ data/datasets/dataset_loader.py
@@ -31,27 +31,17 @@ def do_mosaic(frame, neighbor=8):
     fw, fh = frame.size
     draw = ImageDraw.Draw(frame)
     neighbor = int(neighbor)
-    for i in range(0, fh, int(neighbor)):  # 关键点0 减去neightbour 防止溢出
+    for i in range(0, fh, int(neighbor)):  
         for j in range(0, fw, int(neighbor)):
             r, g, b = frame.getpixel((j, i))
             left_up = (j, i)
             width = min(neighbor, fw - j)
             height = min(neighbor, fh - i)
-            right_down = (j + width - 1, i + height - 1)  # 关键点2 减去一个像素
+            right_down = (j + width - 1, i + height - 1)
             draw.rectangle((left_up, right_down), fill=(r, g, b), outline=None)
 
 
 class Gaussian_noise(object):
-    """增加高斯噪声
-    此函数用将产生的高斯噪声加到图片上
-    传入:
-        img   :  原图
-        mean  :  均值
-        sigma :  标准差
-    返回:
-        gaussian_out : 噪声处理后的图片
-    """
-
     def __init__(self, mean, sigma):
         self.mean = mean
         self.sigma = sigma
@@ -63,18 +53,12 @@ class Gaussian_noise(object):
         Returns:
             PIL Image: PIL image.
         """
-        # 将图片灰度标准化
         img_ = np.array(img).copy()
         img_ = img_ / 255.0
-        # 产生高斯 noise
         noise = np.random.normal(self.mean, self.sigma, img_.shape)
-        # 将噪声和图片叠加
         gaussian_out = img_ + noise
-        # 将超过 1 的置 1，低于 0 的置 0
         gaussian_out = np.clip(gaussian_out, 0, 1)
-        # 将图片灰度范围的恢复为 0-255
         gaussian_out = np.uint8(gaussian_out * 255)
-        # 将噪声范围搞为 0-255
         # noise = np.uint8(noise*255)
         return Image.fromarray(gaussian_out).convert('RGB')
 
